f4175f5822873fc0138f10ffd3c4d70dd1b816bf
Mariam Ayman <121129576+mariam511@users.noreply.github.com>
2025-12-10 01:50:58 +0200

    preprocessing script

---
+++ src/augmentation/preprocessing.py
@@ -0,0 +1,60 @@
+import os
+import cv2
+import numpy as np
+
+processedDir = "data/processed"
+os.makedirs(processedDir, exist_ok=True)
+
+cleanedDir="data/cleaned"
+os.makedirs(cleanedDir,exist_ok=True)
+
+#read cleaned Data and make the classes in processing dir
+classes=[]
+for cls in os.listdir(cleanedDir):
+    path=os.path.join(cleanedDir,cls)
+    if(os.path.isdir(path)):
+        classes.append(cls)
+        classFolderPath=os.path.join(cleanedDir,cls)
+        os.makedirs(classFolderPath,exist_ok=True)
+
+IMG_SIZE=(128,128)       
+
+#Resizing
+for cls in classes:
+    srcFolderPath=os.path.join(cleanedDir,cls)
+    distFolderPath=os.path.join(processedDir,cls)
+    os.makedirs(distFolderPath, exist_ok=True)
+
+
+    for file in os.listdir(srcFolderPath):
+        srcFilePath=os.path.join(srcFolderPath,file)
+        distFilePath=os.path.join(distFolderPath,file)
+
+        img=cv2.imread(srcFilePath)
+        if(img is not None):
+            img_resized=cv2.resize(img,IMG_SIZE)
+            cv2.imwrite(distFilePath,img_resized)
+        else:
+            print("can not read:",srcFilePath)
+            
+# convert files to Numpy arrays
+x = []  
+y = []  
+i=0
+for  cls in (classes):
+    folderPath = os.path.join(processedDir, cls)
+    for file in os.listdir(folderPath):
+        filePath = os.path.join(folderPath, file)
+        img = cv2.imread(filePath) 
+        if img is not None:
+            x.append(img)     
+            y.append(i)      
+        else:
+            print("Cannot read:", filePath)
+x=np.array(x)
+y=np.array(y)
+print("Data shape:", x.shape)
+print("Labels shape:", y.shape)    
+
+
+
